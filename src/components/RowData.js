64c9d0210c02ddf63903b1a1bcb8880904b676b6
PujanZZ <pujan2001@gmail.com>
2022-06-20 17:04:09 +0530

    something done

---
+++ src/components/RowData.js
@@ -0,0 +1,23 @@
+export const Rdata = [
+    {
+        id: 1,
+        name: 'random data',
+        address: 'random data',
+        phoneNumber: '123',
+        email: 'apidata',
+    },
+    {
+        id: 2,
+        name: 'random data',
+        address: 'random data',
+        phoneNumber: '123',
+        email: 'apidata',
+    },
+    {
+        id: 3,
+        name: 'random data',
+        address: 'random data',
+        phoneNumber: '123',
+        email: 'apidata',
+    },
+]
